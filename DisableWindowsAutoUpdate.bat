a621df40373933aa003763a412f3730afd0099e2
Max Edison <amirhossein.heidari.email@gmail.com>
2025-10-23 23:13:50 +0330

    Add Windows Auto Update policies: disable automatic updates

---
+++ DisableWindowsAutoUpdate.bat
@@ -0,0 +1,12 @@
+@echo off
+
+@REM Run as Adminstrator and Reboot to Apply.
+
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v AUOptions /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoRebootWithLoggedOnUsers /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v ScheduledInstallDay /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v ScheduledInstallTime /t REG_DWORD /d 3 /f
+
+
+echo Done!
